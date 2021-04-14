04d3a5f4336d83bbf8cfc55b0cb3f43947509391
FrancoBrechbuhl <francob19051997@gmail.com>
2021-04-13 23:15:27 -0300

    generado de los escenarios predefinidos

--- Aspiradora/src/domain/Escenario.java
+++ Aspiradora/src/domain/Escenario.java
@@ -11,11 +11,151 @@ public class Escenario {
 		this.posiciones = new int[14][9];
 	}
 	
-	public Escenario(String name){
-		this.nombre = name;
+	public void generarEscenario(int nroEsc) {
 		this.posiciones = new int[14][9];
+		
+		//Se inicializa los arboles y los casilleros vacíos
+		
+		//los bordes del escenario son siempre arboles
+    	for (int i = 1; i <= 14; i++) {
+	   		this.posiciones[i][1] = 1;
+    		this.posiciones[i][9] = 1;
+    	}
+		    	
+		for (int j = 1; j <= 9; j++) {
+	   		this.posiciones[1][j] = 1;
+	   		this.posiciones[14][j] = 1;
+    	}
+		
+		//casilleros vacíos
+		for (int i = 2; i <= 13; i++) {
+			for (int j = 2; j <= 8; j++) {
+				this.posiciones[i][j] = 0;
+			}
+		}
+		
+		//generar escenarios predeterminados
+		switch(nroEsc) {
+			case 1: {
+				//camino flores
+				this.posiciones[8][8] = 5;
+				this.posiciones[8][9] = 5;
+				
+				//arboles
+				for (int j = 2; j <= 8; j++) {
+					this.posiciones[2][j] = 1;
+					this.posiciones[3][j] = 1;
+				}
+				
+				this.posiciones[4][5] = 1;
+				this.posiciones[5][3] = 1;
+				this.posiciones[5][5] = 1;
+				this.posiciones[5][6] = 1;
+				this.posiciones[6][6] = 1;
+				this.posiciones[6][7] = 1;
+				this.posiciones[7][7] = 1;
+				this.posiciones[7][8] = 1;
+				this.posiciones[8][2] = 1;
+				this.posiciones[8][7] = 1;
+				this.posiciones[9][5] = 1;
+				this.posiciones[10][4] = 1;
+				this.posiciones[10][7] = 1;
+				this.posiciones[12][2] = 1;
+				this.posiciones[12][7] = 1;
+				this.posiciones[12][8] = 1;
+				
+				for (int j = 2; j <= 8; j++) {
+					this.posiciones[13][j] = 1;
+				}
+				break;
+			}
+			case 2: {
+				//camino flores
+				this.posiciones[7][8] = 5;
+				this.posiciones[7][9] = 5;
+				
+				//arboles
+				for (int j = 2; j <= 8; j++) {
+					this.posiciones[2][j] = 1;
+					this.posiciones[3][j] = 1;
+				}
+				
+				this.posiciones[4][2] = 1;
+				this.posiciones[4][3] = 1;
+				this.posiciones[4][4] = 1;
+				this.posiciones[4][8] = 1;
+				this.posiciones[5][2] = 1;
+				this.posiciones[5][4] = 1;
+				this.posiciones[5][5] = 1;
+				this.posiciones[5][8] = 1;
+				this.posiciones[6][5] = 1;
+				this.posiciones[6][6] = 1;
+				this.posiciones[6][8] = 1;
+				this.posiciones[8][2] = 1;
+				this.posiciones[8][3] = 1;
+				this.posiciones[8][5] = 1;
+				this.posiciones[8][6] = 1;
+				this.posiciones[8][8] = 1;
+				this.posiciones[9][2] = 1;
+				this.posiciones[9][3] = 1;
+				this.posiciones[10][2] = 1;
+				this.posiciones[10][5] = 1;
+				this.posiciones[10][6] = 1;
+				this.posiciones[10][7] = 1;
+				this.posiciones[11][2] = 1;
+				this.posiciones[11][3] = 1;
+				
+				for (int j = 2; j <= 8; j++) {
+					this.posiciones[12][j] = 1;
+					this.posiciones[13][j] = 1;
+				}
+				break;
+				
+			}
+			case 3: {
+				//camino flores
+				this.posiciones[4][1] = 5;
+				this.posiciones[4][2] = 5;
+				
+				//arboles
+				for (int j = 2; j <= 8; j++) {
+					this.posiciones[2][j] = 1;
+					this.posiciones[3][j] = 1;
+				}
+				
+				this.posiciones[4][5] = 1;
+				this.posiciones[4][8] = 1;
+				this.posiciones[5][2] = 1;
+				this.posiciones[5][3] = 1;
+				this.posiciones[5][5] = 1;
+				this.posiciones[5][8] = 1;
+				this.posiciones[6][8] = 1;
+				this.posiciones[7][3] = 1;
+				this.posiciones[8][6] = 1;
+				this.posiciones[8][7] = 1;
+				this.posiciones[9][2] = 1;
+				this.posiciones[10][2] = 1;
+				this.posiciones[10][3] = 1;
+				this.posiciones[10][4] = 1;
+				this.posiciones[10][6] = 1;
+				this.posiciones[10][8] = 1;
+				this.posiciones[11][2] = 1;
+				this.posiciones[11][3] = 1;
+				this.posiciones[11][7] = 1;
+				this.posiciones[11][8] = 1;
+				this.posiciones[12][2] = 1;
+				this.posiciones[12][3] = 1;
+				this.posiciones[12][4] = 1;
+				this.posiciones[12][8] = 1;
+				
+				for (int j = 2; j <= 8; j++) {
+					this.posiciones[13][j] = 1;
+				}
+				break;
+			}
+		}
 	}
-
+	
 	public String getNombre() {
 		return nombre;
 	}
--- Aspiradora/src/frsf/cidisi/exercise/caperucita/search/EstadoAmbiente.java
+++ Aspiradora/src/frsf/cidisi/exercise/caperucita/search/EstadoAmbiente.java
@@ -42,22 +42,9 @@ public class EstadoAmbiente extends EnvironmentState {
 
     	//El estado inicial del estado del Ambiente; el escenario de entrada
     	
-    	//los bordes del escenario son siempre arboles
-    	for (int i = 1; i <= 14; i++) {
-    		escenarioJuego.setPosicionXY(i, 1, 1);
-    		escenarioJuego.setPosicionXY(i, 9, 1);
-    	}
-    	
-    	for (int j = 1; j <= 9; j++) {
-    		escenarioJuego.setPosicionXY(1, j, 1);
-    		escenarioJuego.setPosicionXY(9, j, 1);
-    	}
-    	
-    	for (int i = 2; i <= 13; i++) {
-    		for (int j = 2; j <= 8; j++) {
-    			escenarioJuego.setPosicionXY(i, j, 0);
-    		}
-    	}
+    	//sorteo del escenario
+    	int nroEsc = getRandomN(1,3);
+    	escenarioJuego.generarEscenario(nroEsc);
     	
     	//se setea en el escenario la posición de caperucita
         posicionCaperucita = new Point(getRandomN(2,13), getRandomN(2,8));
@@ -68,31 +55,7 @@ public class EstadoAmbiente extends EnvironmentState {
         	posicionLobo = new Point(getRandomN(2,13), getRandomN(2,8)); //chequeo que no se pare encima de caperucita
         } while(posicionLobo.equals(posicionCaperucita));
         
-        escenarioJuego.setPosicionXY((int)(posicionLobo.getX()), (int)(posicionLobo.getY()), 4);
-        
-        //se setean las posiciones de los arboles
-        /*Esto anda y no anda a la vez, puede llegar a poner arboles de tal forma que 
-          no haya camino posible para llegar a las flores o los dulces, hay que hacerlo
-          de otra forma.
-          La otra seria hacer los escenarios ya predefinidos y no hacerlos random, osea ya de antemano
-          definir tres escenarios que tengan fijos los arboles y los cargamos directamente.
-         */
-        
-        
-        int cantArboles = getRandomN(35,50);
-        boolean repetido;
-        
-        for (int a = 0; a < cantArboles; a++) {
-        	do {
-        		int x = getRandomN(2,13);
-        		int y = getRandomN(2,8);
-        		repetido = true;
-        		if(escenarioJuego.getPosicionXY(x, y) == 0) {
-        			escenarioJuego.setPosicionXY(x, y, 1);
-        			repetido = false;
-        		}
-        	} while(repetido == true);
-        }
+        escenarioJuego.setPosicionXY((int)(posicionLobo.getX()), (int)(posicionLobo.getY()), 4);       	
         
         //TODO: hacer un algoritmo aleatorio para generar los dulces y las flores
         
